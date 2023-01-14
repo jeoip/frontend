26c64959125278308be4e07ff0eff142eeb187f8
Behzad Khanlar <behkha@yahoo.com>
2023-01-14 23:20:45 +0330

    optimize contact us items & update alignments

--- src/components/base/ContactUs.tsx
+++ src/components/base/ContactUs.tsx
@@ -1,13 +1,44 @@
 import ContactUsItem from "./ContactUsItem";
-import styles from '../../styles/ContactUs.module.scss'
+import styles from "../../styles/ContactUs.module.scss";
 
 const ContactUs = () => {
+  const items = [
+    {
+      title: "شماره تماس:",
+      value: "031-34420301",
+      href: "#",
+      icon: "mdi-phone",
+    },
+    {
+      title: "پست الکترونیک",
+      value: "hi@dnj.co.ir",
+      href: "#",
+      icon: "mdi-email",
+    },
+    {
+      title: "پشتیبانی فنی",
+      value: "کلیک کنید",
+      href: "#",
+      icon: "mdi-headset",
+    },
+  ];
+
   return (
-    <div className="d-flex flex-column align-items-center">
-      <p className={`${styles['contact-us__title']}`}>با ما در تماس باشید</p>
-      <ContactUsItem title="شماره تماس:" value="031-34420301" href="#" icon="mdi-phone"/>
-      <ContactUsItem title="پست الکترونیک" value="hi@dnj.co.ir" href="#" icon="mdi-email"/>
-      <ContactUsItem title="پشتیبانی فنی" value="کلیک کنید" href="#" icon="mdi-headset"/>
+    <div>
+      <div className="d-flex flex-row justify-content-center">
+        <p className={`${styles["contact-us__title"]}`}>با ما در تماس باشید</p>
+      </div>
+      <div className="d-flex flex-column align-items-end">
+        {items.map((item, index) => (
+          <ContactUsItem
+            title={item.title}
+            value={item.value}
+            href={item.href}
+            icon={item.icon}
+            key={index}
+          />
+        ))}
+      </div>
     </div>
   );
 };
