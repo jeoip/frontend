8a8c2bafc9eb92a2887b3f1e6a64d86c9ab93174
Behzad Khanlar <behkha@yahoo.com>
2023-01-21 15:52:13 +0330

    add error message & retry button to ip card

--- src/components/IPCard.tsx
+++ src/components/IPCard.tsx
@@ -10,10 +10,12 @@ import IPAPI from "./ip/IPAPI";
 import IPDataTable from "./ip/IPDataTable";
 import IPInformation from "./ip/IPInformation";
 import IPContext from "@/store/IPContext";
+import { FormattedMessage } from "react-intl";
+import styles from '@/styles/IPCard.module.scss'
 
 const Map = dynamic(
   () => {
-    return import('@/components/Map');
+    return import("@/components/Map");
   },
   { ssr: false }
 );
@@ -24,39 +26,67 @@ interface IPCardProps {
 }
 
 const IPCard: React.FC<IPCardProps & Props> = (props) => {
-
-  const ctx = useContext(IPContext)
+  const ctx = useContext(IPContext);
+  const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const getIP = async () => {
+    setError(false);
     setLoading(true);
     try {
       const geolocation = await axios.get("https://geolocation-db.com/json/");
-      props.onIPReady(geolocation.data.IPv4)
-      const response = await axios.get(`https://jeoip.ir/api/${geolocation.data.IPv4}`);
-      props.onIPInformationReady(response)
+      props.onIPReady(geolocation.data.IPv4);
+      const response = await axios.get(
+        `https://jeoip.ir/api/${geolocation.data.IPv4}`
+      );
+      props.onIPInformationReady(response);
     } catch (error) {
+      setError(true);
     } finally {
       setLoading(false);
     }
   };
 
-  useEffect(() => {
+  const retryHandler = () => {
     getIP();
+  }
+
+  useEffect(() => {
+    // getIP();
   }, []);
 
   return (
     <Card className="m-sm-3 px-sm-5 py-3">
       <Brand row={true}></Brand>
       {loading && <Spinner className="mx-auto" />}
-      {!loading && (
+      {!loading && !error && (
         <>
-          <IPInformation className="mx-4 mx-sm-0"/>
+          <IPInformation className="mx-4 mx-sm-0" />
           <IPDataTable className="mt-4 mx-4 mx-sm-0" />
-          {ctx && <Map className="d-block d-sm-none my-3" lat={ctx.latitude} lng={ctx.longitude} />}
+          {ctx && (
+            <Map
+              className="d-block d-sm-none my-3"
+              lat={ctx.latitude}
+              lng={ctx.longitude}
+            />
+          )}
         </>
       )}
-      <IPAPI className="mt-5 mx-4 mx-sm-0"/>
+      {error && (
+        <div className="d-flex flex-column align-items-center mx-4 mx-sm-0">
+          <p className={`${styles['ip-card__error']}`}>
+            <FormattedMessage id="ip.card.error"/>
+          </p>
+          <div className="d-flex flex-row justify-content-center">
+            <FormattedMessage id="ip.card.retry">
+              {(text) => {
+                return <button onClick={retryHandler} className={`${styles['ip-card__retry']}`}>{text}</button>;
+              }}
+            </FormattedMessage>
+          </div>
+        </div>
+      )}
+      <IPAPI className="mt-5 mx-4 mx-sm-0" />
       <FAQ className="mt-5 mx-4 mx-sm-0" />
     </Card>
   );
