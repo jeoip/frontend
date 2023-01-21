f086258ee0c1f1fa5b9a8d59d4329b9eb5f75509
Behzad Khanlar <behkha@yahoo.com>
2023-01-21 17:40:42 +0330

    add ipv6 validation

--- src/components/ip/IPAPI.tsx
+++ src/components/ip/IPAPI.tsx
@@ -206,6 +206,16 @@ const IPAPIInput: React.FC<IPAPIInputProps & Props> = (props) => {
 
   const validateIPaddress = (event: React.ChangeEvent<HTMLInputElement>) => {
     const ip = event.target.value;
+    
+    // IPV6 validation
+    const ipv6Exp = /(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))/gi;
+    if (ipv6Exp.test(ip)) {
+      setIPValid(true)
+      props.onEnterIP(ip);
+      return
+    }
+
+    // IPV4 validation
     const blocks = ip.split(".");
     if (!ip) {
       setIPValid(null);
@@ -229,6 +239,7 @@ const IPAPIInput: React.FC<IPAPIInputProps & Props> = (props) => {
         break;
       }
     }
+
     setIPValid(valid);
     props.onEnterIP(valid ? ip : "");
   };
