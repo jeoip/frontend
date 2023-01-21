4d98a825e105fea602ffe604080ef1a59b825842
Behzad Khanlar <behkha@yahoo.com>
2023-01-21 15:08:47 +0330

    intl faq items

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,6 +1,6 @@
 import Props from "@/types/Props";
 import styles from "@/styles/FAQ.module.scss";
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, useIntl } from "react-intl";
 import { getDirection } from "@/lang/locale";
 
 export interface FAQItemProps {
@@ -18,29 +18,19 @@ const FAQItem: React.FC<Props & FAQItemProps> = (props) => {
 }
 
 const FAQ: React.FC<Props> = (props) => {
-  const items = [
-    {
-      question: "چجوری میتونم مشخص کنم از کدام ورژن از آی پی استفاده شود؟",
-      answer:
-        "از سال 2018-07-25 مشخص کردن ورژن آی پی در پروتوکل ها منسوخ شده و امکان پذیر نمیباشد. ولیکن میتوانید در صورت داشتن امکان با استفاده از پارامتر درست در برنامه‌ای که استفاده میکنید، ورژن آی پی را تعیین کنید. برای مثال در فرمان curl میتوانید با پارامتر های 4- و 6- مشخص کنید از کدام ورژن آی پی که دارید استفاده شود.",
-    },
-    {
-      question: "چگونه میتوانم این اطلاعات رو به صورت JSON دریافت کنم؟",
-      answer:
-        "با تنظیم پارامتر هدر درخواست برابر application/json میتوانید اطلاعات را به صورت JSON دریافت کنید.",
-    },
-    {
-      question: "آیا استفاده از این ابزار در برنامه ها مجاز است؟",
-      answer: `بله، تا زمانی که تعداد درخواست های شما از حد معینی بیشتر نشود و فشار بر روی سرور های ما ایجاد نکند، میتوانید از این ابزار در برنامه ها استفاده کنید.
-      در برنامه هایتان سعی کنید در هر دقیقه بیشتر از یک درخواست ارسال نکنید. در صورتیکه درخواست های شما بیشتر شود، تا مدتی از ایجاد درخواست جدید از طرف شما جلوگیری خواهد شد.`,
-    },
-    {
-      question:
-        "آیا میتوانم به صورت اختصاصی این ابزار را برای خودم راه اندازی کنم؟",
-      answer:
-        "بله، میتوانید با استفاده از شماره های تماس با ما در ارتباط باشید و ما ابزار اختصاصی خودتان را راه اندازی خواهیم کرد.",
-    },
-  ];
+  const intl = useIntl()
+  const questions = intl.messages['faq.questions']
+  const answers = intl.messages['faq.answers']
+  if (questions.length != answers.length) {
+    return (<></>)
+  }
+  const items = []
+  for (let i = 0; i < questions.length; i++) {
+    items.push({
+      question: questions[i].toString(),
+      answer: answers[i].toString()
+    })
+  }
 
   const iconRotation = (getDirection() === 'rtl') ? 'mdi-rotate-315' : 'mdi-rotate-135'
 
